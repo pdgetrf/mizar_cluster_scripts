dc8441fd2b29dfa11cee8311d6edf97b6c04460c
root <root@ip-172-31-7-100.us-west-2.compute.internal>
2021-09-08 20:29:11 +0000

    optimization: parallelize slave operations

--- create_vpc_nets.sh
+++ create_vpc_nets.sh
@@ -2,15 +2,15 @@
 
 set -e
 
+# 
+# verify vpc0 is already provisioned
 #
-# verify vpc0 is provisioned
-#
-vpc0=""
-net0=""
+vpc0=`kubectl get vpc vpc0|awk '{print $6}'`
+net0=`kubectl get subnet net0|awk '{print $6}'`
 while [[ "$vpc0" != *"Provisioned"* || "$net0" != *"Provisioned"* ]]
 do
-  echo "waiting for vpc0 to be provisioned"
-  sleep 1
+  echo ">> waiting 5 seconds for vpc0 to be provisioned"
+  sleep 5 
   vpc0=`kubectl get vpc vpc0|awk '{print $6}'`
   net0=`kubectl get subnet net0|awk '{print $6}'`
 done
@@ -29,7 +29,7 @@ kubectl create -f ./test_vpc1.yaml
 vni=""
 while [ -z "$vni" ]
 do
-  echo "wait for VNI from vpc1"
+  echo ">> wait for VNI from vpc1"
   sleep 1
   vni=$(kubectl get vpc vpc1|grep vpc1|awk '{print $4}')
 done
@@ -37,7 +37,7 @@ done
 #
 # replace with the right VNI
 #
-echo "setting VNI to $vni"
+echo ">> setting VNI to $vni for subnets"
 cat test_net1.yaml.template|sed "s/VPC_VNI/$vni/" > net1.yaml 
 cat test_net2.yaml.template|sed "s/VPC_VNI/$vni/" > net2.yaml 
 
--- restart_cluster.sh
+++ restart_cluster.sh
@@ -7,8 +7,9 @@ kubeadmresetlog=/tmp/kubeadm_reset.out
 kubeadminitlog=/tmp/kubeadm_init.out
 kubeadmjoinlog=/tmp/kubeadm_join.out
 mizarlog=/tmp/mizar.out
+secondstowait=10
 
-echo "---- verifying slave host file exist ----"
+echo ">> verifying slave host file exist"
 if [ ! -f "$slavehosts" ]; then
     echo "$slavehosts does not exist."
     echo "put IPs of slave hosts in a file called **slave.in** and try again."
@@ -18,35 +19,54 @@ fi
 
 rm -f /tmp/*.out
 
-echo "---- resetting and cleaning up slave nodes ----"
+nuke_slave(){
+   slave=$1
+   ssh -n $slave "kubeadm reset -f" > $kubeadmresetlog 2>&1 
+   scp ./cleanup.sh $slave:/tmp > $cleanuplog 2>&1
+   ssh -n $slave "/tmp/cleanup.sh" >> $cleanuplog 2>&1
+}
+
+echo ">> resetting and cleaning up slave nodes (in parallel)"
 while IFS= read -r slave
 do
-    echo "-- nuking slave $slave --"
-    ssh -n $slave "kubeadm reset -f"|tee $kubeadmresetlog
-    scp ./cleanup.sh $slave:/tmp
-    ssh -n $slave "/tmp/cleanup.sh"|tee $cleanuplog
+    echo ">>>> nuking slave $slave"
+    nuke_slave $slave & # remove & here to see log in its entirety
 done < "$slavehosts"
+wait
 
-echo "---- resetting master node----"
-kubeadm reset -f | tee $kubeadmresetlog 
+echo ">> resetting master node"
+kubeadm reset -f | tee $kubeadmresetlog > $kubeadmresetlog 2>&1 
 
-echo "---- starting master node----"
-kubeadm init --pod-network-cidr 20.0.0.0/16|tee $kubeadminitlog 
+echo ">> starting master node"
+kubeadm init --pod-network-cidr 20.0.0.0/16 > $kubeadminitlog 2>&1
 
-echo "---- joining slave nodes ----"
+echo ">> joining slave nodes (in parallel)"
 token=`cat $kubeadminitlog|grep "\-\-token "|awk '{print $5}'`
 master=`cat $kubeadminitlog|grep "\-\-token "|awk '{print $3}'`
 certhash=`cat $kubeadminitlog|grep "discovery-token-ca-cert-hash"|awk '{print $2}'`
 joincmd="kubeadm join $master --token $token --discovery-token-ca-cert-hash $certhash"
 
-echo "joining with '$joincmd'"
+echo ">>>> joining with '$joincmd'"
 
 while IFS= read -r slave
 do
-    echo "-- joining from $slave --"
-    ssh -n $slave "$joincmd"|tee $kubeadmjoinlog
+    echo ">>>> joining from $slave"
+    ssh -n $slave "$joincmd" > $kubeadmjoinlog 2>&1 & # remove & here to see log in its entirety
 done < "$slavehosts"
+wait 
 
-echo "---- installing mizar ----"
-kubectl create -f mizar.dev.yaml|tee $mizarlog
+echo ">> installing mizar"
+kubectl create -f mizar.dev.yaml > $mizarlog 2>&1
 
+echo ">> verifying vpc0 is provisioned"
+vpc0=""
+net0=""
+while [[ "$vpc0" != *"Provisioned"* || "$net0" != *"Provisioned"* ]]
+do
+  echo ">>>> not yet. waiting $secondstowait seconds for vpc0 and its subnet0 to be provisioned"
+  sleep $secondstowait 
+  vpc0=`kubectl get vpc vpc0|awk '{print $6}'`
+  net0=`kubectl get subnet net0|awk '{print $6}'`
+done
+
+echo "ALL DONE! YEEHAW!!"
