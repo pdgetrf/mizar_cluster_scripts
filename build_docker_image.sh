fce53c63d4d35f7b12b4c03386c3067a3e672794
root <root@ip-172-31-7-100.us-west-2.compute.internal>
2021-09-13 05:05:21 +0000

    finish build_docker_image.sh

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-set -x
 
 tag=goose
 mizardir=/root/mizar
@@ -10,6 +9,7 @@ slavehosts="./slaves.in"
 buildlog="/tmp/build_image.out"
 declare -a components=("mizar" "dropletd" "endpointopr")
 declare -a dockerfiles=("mizar.Dockerfile" "daemon.Dockerfile" "operator.Dockerfile")
+imagenamefile="/tmp/imgfiles.in"
 
 build_image(){
     echo ">>>> building images"
@@ -17,10 +17,17 @@ build_image(){
     rm -f $buildlog >> $buildlog 2>&1
     pushd $mizardir >> $buildlog 2>&1
     length=${#dockerfiles[@]}
+        
+    echo "compiling mizar"
+    make clean >> $buildlog 2>&1
+    make all >> $buildlog 2>&1
+
     for (( i=0; i<${length}; i++ ));
     do
-        echo building vmizarnet/${components[$i]} using $dockerfilepath 
         dockerfilepath="$mizarbuilddir/${dockerfiles[$i]}"
+        
+	echo building vmizarnet/${components[$i]} using $dockerfilepath 
+
 	image="vmizarnet/${components[$i]}:$tag"
 	docker rmi -f $image >> $buildlog 2>&1
 	docker image build -t $image -f $dockerfilepath . >> $buildlog 2>&1
@@ -29,49 +36,71 @@ build_image(){
     popd 2>&1
 }
 
+zip_image_fn(){
+    component=$1
+    image="vmizarnet/$component:$tag"
+    file="./$component.tar"
+    echo saving $image to $file 
+
+    rm -f $file
+    docker save -o $file $image 
+    gzip -f ${file} 
+}
+
 zip_image(){
     echo ">>>> zipping images"
     length=${#dockerfiles[@]}
     for (( i=0; i<${length}; i++ ));
     do
-	image="vmizarnet/${components[$i]}:$tag"
-	file="./${components[$i]}.tar"
-        echo saving $image to ${components[$i]}.tar 
-	rm -f $file
-	docker save -o $file $image 
-	gzip -f ${file}
+        zip_image_fn ${components[$i]} &
     done
+    wait
+}
+
+send_file_names(){
+    echo ">>>> reloading images on slave $slave"
+    rm -f $imagenamefile
+    length=${#components[@]}
+    for (( i=0; i<${length}; i++ ));
+    do
+        echo ${components[$i]} >> $imagenamefile 
+    done
+
+    while IFS= read -r slave
+    do
+        scp $imagenamefile $slave:/tmp >> $buildlog $2>&1
+    done < "$slavehosts"
 }
 
 # ref https://stackoverflow.com/questions/22107610/shell-script-run-function-from-script-over-ssh
 slave_reload_fn(){
     slavereloadlog=/tmp/reload.out
     rm -f $slavereloadlog
+    imagenamefile=/tmp/imgfiles.in
+    tag=$1
+
     gzip -d *.gz >> $slavereloadlog 2>&1
-    docker rmi -f vmizarnet/mizar:$tag >> $slavereloadlog 2>&1
-    docker rmi -f vmizarnet/dropletd:$tag >> $slavereloadlog 2>&1
-    docker rmi -f vmizarnet/endpointopr:$tag >> $slavereloadlog 2>&1
-    docker load -i ./dr.tar >> $slavereloadlog 2>&1
-    docker load -i ./ep.tar >> $slavereloadlog 2>&1
-    docker load -i ./mz.tar >> $slavereloadlog 2>&1
+
+    while IFS= read -r component 
+    do
+        echo "reloading vmizarnet/$component:$tag with ./$component.tar"
+	docker rmi -f vmizarnet/$component:$tag >> $slavereloadlog 2>&1
+	docker load -i /root/$component.tar >> $slavereloadlog 2>&1
+    done < "$imagenamefile"
+    
+    mv -f *.tar /tmp >> $slavereloadlog 2>&1
+    mv -f *.gz /tmp >> $slavereloadlog 2>&1
 }
 
 send_and_reload(){
     slave=$1
     echo ">>>> reloading images on slave $slave"
-    scp *.tar.gz $slave:~
-    ssh $slave "$(typeset -f slave_reload_fn); slave_reload_fn"
+
+    scp *.tar.gz $slave:~ >> $buildlog 2>&1
+    ssh $slave "$(typeset -f slave_reload_fn); slave_reload_fn $tag"
 }
 
 distribute_and_reload_images(){
-    echo ">> verifying slave host file exist"
-    if [ ! -f "$slavehosts" ]; then
-        echo "$slavehosts does not exist."
-        echo "put IPs of slave hosts in a file called **slave.in** and try again."
-        echo "exited on purpose."
-        exit
-    fi
-    
     while IFS= read -r slave
     do
 	send_and_reload $slave &
@@ -79,6 +108,18 @@ distribute_and_reload_images(){
     wait
 }
 
+echo ">> verifying slave host file exist"
+if [ ! -f "$slavehosts" ]; then
+    echo "$slavehosts does not exist."
+    echo "put IPs of slave hosts in a file called **slave.in** and try again."
+    echo "exited on purpose."
+    exit
+fi
+    
 build_image
+
 zip_image
-#distribute_and_reload_images
+
+send_file_names
+
+distribute_and_reload_images
