2a38359d8b778d0ecc7c43df16c010014b8bd91d
root <root@ip-172-31-7-100.us-west-2.compute.internal>
2021-09-28 16:57:26 +0000

    some text changes in ping scripts

--- ping_0.sh
+++ ping_0.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+set -e
+
+# this pings the pod in the .0 subnet from the pod in the .122 subnet
+
 to=`kubectl get pods -o wide|grep pod-in-net1|awk '{print $6}'`
 from=`kubectl get pods -o wide|grep pod-in-net2|awk '{print $6}'`
+echo "-------"
 echo "pinging $to from $from" 
+echo "-------"
 kubectl exec -it pod-in-net2 -- /bin/ping $to
